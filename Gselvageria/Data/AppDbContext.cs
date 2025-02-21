88a8e739658b33952e55a02e6c4948ef82e38790
gui777x <guizin.clementino@gmail.com>
2025-02-21 11:23:20 -0300

    Criando o Contexto

---
+++ Gselvageria/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+using Gselvageria.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gselvageria.Data;
+
+public class AppDbContext : IdentityDbContext<Usuario>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }   
+
+    public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<Produto> Produtos { get; set; }
+    public DbSet<ProdutoFoto> ProdutoFotos { get; set; }
+    public DbSet<Usuario> Usuarios { get; set; }
+}
