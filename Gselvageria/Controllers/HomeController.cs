268dd15e634ed6035b231d9dd2df3eac4a4d8bca
gui777x <guizin.clementino@gmail.com>
2025-03-18 11:42:16 -0300

    Deixando a pagina inicial dinamica

--- Gselvageria/Controllers/HomeController.cs
+++ Gselvageria/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Gselvageria.Models;
+using Gselvageria.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Gselvageria.Controllers;
 
@@ -8,14 +10,22 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    private readonly AppDbContext _db;
+
+    public HomeController(ILogger<HomeController> logger, AppDbContext db)
     {
         _logger = logger;
+        _db = db;
     }
 
     public IActionResult Index()
+
     {
-        return View();
+        List<Produto> produtos = _db.Produtos
+        .Where(p => p.Destaque)
+        .Include(p => p.Fotos)
+        .ToList();
+        return View(produtos);
     }
 
     public IActionResult Privacy()
