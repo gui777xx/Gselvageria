fb5eeb16466dd01512659b661d909fa9733c309a
gui777x <guizin.clementino@gmail.com>
2025-05-26 10:49:44 -0300

    Parei na pagina 28 quase no final

--- Gselvageria/Controllers/AccountController.cs
+++ Gselvageria/Controllers/AccountController.cs
@@ -1,7 +1,10 @@
+using System.Net.Mail;
+using System.Security.Claims;
 using Gselvageria.Models;
+using Gselvageria.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Gselvageria.ViewModels;
+
 
 namespace Gselvageria.Controllers;
 
@@ -32,5 +35,45 @@ public class AccountController : Controller
             UrlRetorno = returnUrl ?? Url.Content("~/"),
         };
         return View(login);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Login(LoginVM login)
+    {
+        if (!ModelState.IsValid)
+        {
+           string userName = login.Email;
+           if (IsValidEmail(login.Email))
+           {
+               var user = await _userManager.FindByEmailAsync(login.Email);
+               if (user != null)
+                    userName = user.UserName;
+           }
+
+        var result = await _signInManager.PasswordSignInAsync(
+            userName, login.Senha, login.Lembrar, lockoutOnFailure: true
+        );
+
+        if (result.Succeeded) {
+            _logger.LogInformation($"Usuário {login.Email} acessou o sistema");
+            return LocalRedirect(login.UrlRetorno);
+        }
+        if (result.IsLockedOut)
+        {
+            _logger.LogWarning($"Usuário {login.Email} está bloqueado");
+            ModelState.AddModelError(string.Empty, "Suaconta não está confirmada, verifique seu Email!!");
+        }
+    public bool IsValidEmail(string email)
+    {
+        try
+        {
+            MailAddress mail = new(email);
+            return true;
+        }
+        catch (FormatException)
+        {
+            return false;
+        }
     }
 }
