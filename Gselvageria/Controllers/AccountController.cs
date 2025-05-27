51f73370c97505b9bcea5923402d872ca05b7c81
gui777x <guizin.clementino@gmail.com>
2025-05-27 11:54:13 -0300

    feito

--- Gselvageria/Controllers/AccountController.cs
+++ Gselvageria/Controllers/AccountController.cs
@@ -5,15 +5,14 @@ using Gselvageria.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
-
 namespace Gselvageria.Controllers;
-
 public class AccountController : Controller
 {
     private readonly ILogger<AccountController> _logger;
     private readonly SignInManager<Usuario> _signInManager;
     private readonly UserManager<Usuario> _userManager;
     private readonly IWebHostEnvironment _host;
+
     public AccountController(
         ILogger<AccountController> logger,
         SignInManager<Usuario> signInManager,
@@ -32,7 +31,7 @@ public class AccountController : Controller
     {
         LoginVM login = new()
         {
-            UrlRetorno = returnUrl ?? Url.Content("~/"),
+            UrlRetorno = returnUrl ?? Url.Content("~/")
         };
         return View(login);
     }
@@ -41,29 +40,54 @@ public class AccountController : Controller
     [ValidateAntiForgeryToken]
     public async Task<IActionResult> Login(LoginVM login)
     {
-        if (!ModelState.IsValid)
+        if (ModelState.IsValid)
         {
-           string userName = login.Email;
-           if (IsValidEmail(login.Email))
-           {
-               var user = await _userManager.FindByEmailAsync(login.Email);
-               if (user != null)
+            string userName = login.Email;
+            if (IsValidEmail(login.Email))
+            {
+                var user = await _userManager.FindByEmailAsync(login.Email);
+                if (user != null)
                     userName = user.UserName;
-           }
+            }
 
-        var result = await _signInManager.PasswordSignInAsync(
-            userName, login.Senha, login.Lembrar, lockoutOnFailure: true
-        );
+            var result = await _signInManager.PasswordSignInAsync(
+                userName, login.Senha, login.Lembrar, lockoutOnFailure: true
+            );
 
-        if (result.Succeeded) {
-            _logger.LogInformation($"Usuário {login.Email} acessou o sistema");
-            return LocalRedirect(login.UrlRetorno);
+            if (result.Succeeded)
+            {
+                _logger.LogInformation($"Usuário {login.Email} acessou o sistema");
+                return LocalRedirect(login.UrlRetorno);
+            }
+            if (result.IsLockedOut)
+            {
+                _logger.LogWarning($"Usuário {login.Email} está bloqueado");
+                ModelState.AddModelError("", "Sua conta está bloqueada, aguarde alguns minutos e tente novamente!!");
+            }
+            else
+            if (result.IsNotAllowed)
+            {
+                _logger.LogWarning($"Usuário {login.Email} está bloqueado");
+                ModelState.AddModelError(string.Empty, "sua conta não está confirmada, verifique seu email!!");
+
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Usuário e/ou senha Invalidos!!!");
+            }
         }
-        if (result.IsLockedOut)
-        {
-            _logger.LogWarning($"Usuário {login.Email} está bloqueado");
-            ModelState.AddModelError(string.Empty, "Suaconta não está confirmada, verifique seu Email!!");
-        }
+        return View(login);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Logout()
+    {
+        _logger.LogInformation($"Usuário {ClaimTypes.Email} fez logoff");
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Index", "Home");
+    }
+
     public bool IsValidEmail(string email)
     {
         try
